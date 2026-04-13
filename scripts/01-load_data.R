947c9139ae18103fc74fff7432e27f56cc2e0c22
ellibrown <ellibrown10@gmail.com>
2026-04-13 12:30:23 +0200

    cleaned up for reproducibility

--- scripts/01-load_data.R
+++ scripts/01-load_data.R
@@ -13,7 +13,7 @@ median_income <- get_cansim("11-10-0008-01")
 saveRDS(median_income, "data_raw/raw_median_income.rds")
 
 # filter and save MBM threshold data
-mbm_thresholds_filtered <- raw_mbm_thresholds %>%
+mbm_thresholds_filtered <- mbm_thresholds %>%
   filter(REF_DATE == 2023 & Component == "Total threshold" & `Base year`=="2023 base") %>%
   select(REF_DATE, GEO, `Dollar concept`, Component, VALUE) %>%
   filter(`Dollar concept` == "Current dollars")%>%
@@ -23,7 +23,7 @@ mbm_thresholds_filtered <- raw_mbm_thresholds %>%
 saveRDS(mbm_thresholds_filtered, "data_preprocessed/mbm_thresholds.rds")
 
 # filter and save median income data
-median_income_filtered <- raw_median_income %>%
+median_income_filtered <- median_income %>%
   filter(REF_DATE == 2023 & `Persons with income` == "Median total income" 
          & `Sex` == "Both sexes" & `Age group` == "All age groups") %>%
   select(REF_DATE, GEO, VALUE) %>%
